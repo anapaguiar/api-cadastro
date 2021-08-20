7acf6b7790ebe91a07cad9d1758bcbaaf2d5cb41
Ana Paula <86925882+anapaguiar@users.noreply.github.com>
2021-08-20 14:52:20 -0300

    Update TokenController.php

--- app/Http/Controllers/TokenController.php
+++ app/Http/Controllers/TokenController.php
@@ -7,23 +7,23 @@ use Firebase\JWT\JWT;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Hash;
 
+use \LaravelLegends\PtBrValidator\Rules\Cpf;
+
 class TokenController extends Controller {
     
     public function gerarToken(Request $request)
     {
-        //campos e regras de acesso
+
         $this->validate($request, [
             'email' => 'required|email',
             'password' => 'required',
             'cpf' => 'required'
         ]);
 
-        //pega o usuario do request
         $usuario = User::where(
             'email', $request->email)
             ->first();
         
-        //confere o usuario e a senha
         if (is_null($usuario) 
             || !Hash::check($request->password, $usuario->password)
             ) {
